688bdc7687112d01c8482a4b1bae936ca9d643a0
hj8853 <hj885353@gmail.com>
2020-07-06 19:04:56 +0900

    Add url,model,templates,namespace

--- review/views.py
+++ review/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-from datetime import datetime
+from .models import Question
 
 # Create your views here.
 def index(request):
-    now = datetime.now
+    question_list = Question.objects.order_by("-create_date")  
     context = {
-        "current_date":now
+        "question_list":question_list
     }
-    return render(request, "review/index.html", context)
+    return render(request, "review/question_list.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question":question
+    }
+    return render(request, "review/question_detail.html", context)
